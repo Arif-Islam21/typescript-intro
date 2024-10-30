9d0203fe82f0dc544f77269a863a35a739ee30fa
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-10-30 16:00:42 +0600

    moving to typescript

--- index.js
+++ index.js
@@ -6,17 +6,38 @@ const menu = [
 ];
 
 let cashInRegister = 100;
+let nextOrderId = 1;
 const orderQueue = [];
 
 function addNewPizza(pizzaObject) {
-  //   const pizzaObject = { name: "pizza Name", price: 8 };
   menu.push(pizzaObject);
 }
 
 function placeOrder(pizzaName) {
   const selectedPizza = menu.find((item) => item.name === pizzaName);
   cashInRegister += selectedPizza.price;
-  const newOrder = { pizza: selectedPizza, status: "Ordered" };
+  const newOrder = {
+    id: nextOrderId++,
+    pizza: selectedPizza,
+    status: "Ordered",
+  };
   orderQueue.push(newOrder);
   return newOrder;
 }
+
+function completeOrder(orderId) {
+  const order = orderQueue.find((order) => order.id === orderId);
+  order.status = "Completed";
+  return order;
+}
+
+addNewPizza({ name: "chikken", price: 12 });
+addNewPizza({ name: "BBQ", price: 10 });
+addNewPizza({ name: "Spicy", price: 11 });
+
+placeOrder("chikken");
+completeOrder(1);
+
+console.log("menu", menu);
+console.log("cash", cashInRegister);
+console.log("Order Queue", orderQueue);
