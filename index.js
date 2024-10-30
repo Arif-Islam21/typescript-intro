6d680755372b067d62d27ca64d94827127d5a224
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-10-30 15:48:27 +0600

    exploreing

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-const menu = [];
+const menu = [
+  { name: "margharita", price: 8 },
+  { name: "pepperium", price: 12 },
+  { name: "quora", price: 10 },
+  { name: "mangito", price: 9 },
+];
+
+let cashInRegister = 100;
+const orderQueue = [];
+
+function addNewPizza(pizzaObject) {
+  //   const pizzaObject = { name: "pizza Name", price: 8 };
+  menu.push(pizzaObject);
+}
+
+function placeOrder(pizzaName) {
+  const selectedPizza = menu.find((item) => item.name === pizzaName);
+  cashInRegister += selectedPizza.price;
+  const newOrder = { pizza: selectedPizza, status: "Ordered" };
+  orderQueue.push(newOrder);
+  return newOrder;
+}
