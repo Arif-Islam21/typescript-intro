eee2a8feb3c014a258537b76f60a4ffa5e8477d5
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-11-01 17:28:55 +0600

    lets move on nested object types

--- tndex.ts
+++ tndex.ts
@@ -1,3 +1,23 @@
-let myName: string = "Arif";
-const numberOfWheels: number = 5;
-const isStudent: boolean = true;
+// CUSTOM TYPE
+// type Food = string;
+
+// let favouriteFood: Food = "pizza";
+
+// CUSTOM TYPE OBJECT
+
+type Person = {
+  name: string;
+  age: number;
+  isStudent: boolean;
+};
+
+let person: Person = {
+  name: "arif",
+  age: 20,
+  isStudent: true,
+};
+let person2: Person = {
+  name: "Tarik",
+  age: 66,
+  isStudent: false,
+};
