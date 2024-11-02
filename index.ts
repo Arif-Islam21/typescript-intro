94ed30a536ddccca9fb4963066368db5e98d2e31
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-11-02 13:09:34 +0600

    not understood

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ type Pizza = {
 type Order = {
   id: number;
   pizza: Pizza;
-  status: string;
+  status: "Ordered" | "Completed";
 };
 
 const menu = [
@@ -31,7 +31,7 @@ function placeOrder(pizzaName: string) {
     return;
   }
   cashInRegister += selectedPizza.price;
-  const newOrder = {
+  const newOrder: Order = {
     id: nextOrderId++,
     pizza: selectedPizza,
     status: "Ordered",
