2e5fe151f6f1f293d6bf195dd0b5500e45b93685
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-11-02 11:27:32 +0600

    learning optional types

--- tndex.ts
+++ tndex.ts
@@ -4,20 +4,36 @@
 // let favouriteFood: Food = "pizza";
 
 // CUSTOM TYPE OBJECT
+type Address = {
+  street: string;
+  city: string;
+  country: string;
+};
 
 type Person = {
   name: string;
   age: number;
   isStudent: boolean;
+  address: Address;
 };
 
 let person: Person = {
   name: "arif",
   age: 20,
   isStudent: true,
+  address: {
+    street: "Mokterpara",
+    city: "Nilphamari",
+    country: "Bangladesh",
+  },
 };
 let person2: Person = {
   name: "Tarik",
   age: 66,
   isStudent: false,
+  address: {
+    street: "Mokterpara",
+    city: "Nilphamari",
+    country: "Bangladesh",
+  },
 };
