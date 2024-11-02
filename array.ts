d108e612fccdd05111581283aa225c4f8ee9beef
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-11-02 11:47:18 +0600

    learned about array type

---
+++ array.ts
@@ -0,0 +1,18 @@
+type Person = {
+  name: string;
+  age: number;
+  isStudent: boolean;
+};
+
+let person1: Person = {
+  name: "arif",
+  age: 20,
+  isStudent: true,
+};
+let person2: Person = {
+  name: "sojib",
+  age: 29,
+  isStudent: false,
+};
+
+let People: Person[] = [person1, person2];
--- tndex.ts
+++ tndex.ts
@@ -10,28 +10,28 @@ type Address = {
   country: string;
 };
 
-type Person = {
-  name: string;
-  age: number;
-  isStudent: boolean;
-  address?: Address;
-};
+// type Person = {
+//   name: string;
+//   age: number;
+//   isStudent: boolean;
+//   address?: Address;
+// };
 
-let person1: Person = {
-  name: "arif",
-  age: 20,
-  isStudent: true,
-};
-let person2: Person = {
-  name: "Tarik",
-  age: 66,
-  isStudent: false,
-  address: {
-    street: "Mokterpara",
-    city: "Nilphamari",
-    country: "Bangladesh",
-  },
-};
+// let person1: Person = {
+//   name: "arif",
+//   age: 20,
+//   isStudent: true,
+// };
+// let person2: Person = {
+//   name: "Tarik",
+//   age: 66,
+//   isStudent: false,
+//   address: {
+//     street: "Mokterpara",
+//     city: "Nilphamari",
+//     country: "Bangladesh",
+//   },
+// };
 
 function displayInfo(person: Person) {
   console.log(`${person.name} lives at ${person?.address?.street}`);
